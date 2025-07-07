796fc5155b3ecc9e6110c3be4959ab1251e0f0b5
Missnine99 <annmissnine@gmail.com>
2025-07-07 15:45:58 -0400

    Delete test_scoring.py

--- test_scoring.py
+++
@@ -1,6 +0,0 @@
-from vlrk.scoring import calculate_score
-
-def test_score():
-    config = {'threshold': 5, 'weights': {'fraud_warning': 3}}
-    issues = {'fraud_warning': 2}
-    assert calculate_score(issues, config) == 6
