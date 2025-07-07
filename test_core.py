7fa725d4df6c8b3c0677dc378a7a25cb5b9d9a47
Missnine99 <annmissnine@gmail.com>
2025-07-07 15:45:48 -0400

    Delete test_core.py

--- test_core.py
+++
@@ -1,8 +0,0 @@
-from vlrk.core import VLRKMonitor
-
-def test_basic_flow():
-    monitor = VLRKMonitor(config_path="config/default_config.yml")
-    monitor.record_issue("login_timeout")
-    monitor.record_issue("login_timeout")
-    monitor.record_issue("fraud_warning")
-    monitor.check_anomalies()
